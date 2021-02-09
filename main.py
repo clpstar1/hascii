d036b56a17d30085f6615bce3846f5aa5cdd6e80
Steffen Lesch <steffen.lesch96@gmail.com>
2021-02-09 23:03:34 +0100

    add commented out experimental feature

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ from split import splitH, splitV
 
 
 def compress(array, blockdim, size): 
-     
+        
     x, y = blockdim 
     w, h = size 
 
     h_split = splitH(array, w * y)
-    
+
     res = []
     for subl in h_split:
 
         v_split = splitV(subl, w, y)
         for subsubl in v_split:
             res.append(sum(subsubl) / len(subsubl))
-    
+
     return res 
 
-def print_lum(val, op, compare):
-    if op(val, compare):
+def print_lum(lum, op, compare):
+    if op(lum, compare):
         print('.', end='')
     else:
         print('#', end='')
@@ -47,7 +47,7 @@ def get_divs(x_orig):
     for rowsz in range(2, int(x_orig / 2)):
         if x_orig % rowsz == 0:
             divisors.append(rowsz)
-    
+
     return divisors
 
 def crop_even(img):
@@ -72,27 +72,54 @@ if __name__ == '__main__':
 
     # convert to black/white
     img = Image.open(args.filename).convert('LA')
-    
+
     img = crop_even(img)
 
     x, y = img.size
 
-    aspectRatio = x / y
+    aspectRatio = x / y  
 
     divs = get_divs(x)
+    fil = lambda f : list(filter(lambda f : f > 40 and f < 300, f))
+    mapp = lambda div : int(x / div) 
+
+    rowses = fil(map(mapp, divs))
+
+    tries = 10 
+    while rowses == [] and tries > 0:
+        tries -= 1
+        x, y = img.size 
+        x -= 2
+        y -= 2
+        img = img.crop((0, 0, x, y))
+        x, y = img.size
+        divs = get_divs(x)
+        rowses = fil(map(mapp, divs))
+
+    # luminance = list(map(lambda data: data[0], img.getdata()))
 
     luminance = list(map(lambda data: data[0], img.getdata()))
 
-    for div in divs:
+    '''
+    keep = True 
+    count = 0
+    res = []
+    for data in img.getdata():
+        count += 1 
+        if count % x == 0:
+            keep = not keep 
+        if keep:
+            res.append(data[0])
 
-        rows = int(x / div) 
+    y = y / 2
+    '''
+    
+    for rows in rowses:
+        cols = int(rows * (1/aspectRatio))
+        xdim = int(x / rows)
+        ydim = int(y / cols)
 
-        if rows > 40:
-            cols = int(rows * (1/aspectRatio))
-            
-            xdim = int(x / rows)
-            ydim = int(y / cols)
+        
+        res = compress(luminance, (xdim, ydim), (x, y))
 
-            res = compress(list(luminance), (xdim, ydim), (x, y))
-
-            print_ascii(res, args.i, rows, int(args.c))
+        print_ascii(res, args.i, rows, int(args.c))
